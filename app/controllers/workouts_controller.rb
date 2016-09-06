004d02d9dbf18442a4cf9b7b823fe7413790bc88
Julio-Home <julio.ss@gmail.com>
2016-09-06 10:58:48 -0300

    shows workout view

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,9 @@
 class WorkoutsController < ApplicationController
 
   def show
-    @workout = Workout.find(params[:id])
+    @exercise = Exercise.find_by(workout_id: params[:id])
+    @exercise_sets = ExerciseSet.where(exercise_id: @exercise.id).all
+    puts "$$$$$$$$$$$$$s$"
+    puts @exercise_sets
   end
 end 
