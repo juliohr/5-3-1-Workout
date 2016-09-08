f0201d6ff5026ba76226cb2ec5f3a948fd0ce982
Julio-Home <julio.ss@gmail.com>
2016-09-08 13:24:17 -0300

    refactors model exercise set spec

--- spec/models/exercise_set_spec.rb
+++ spec/models/exercise_set_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ExerciseSet, type: :model do
-  it "is valid with valid attributes" do
-  	expect(FactoryGirl.build(:exercise_set)).to be_valid
-  end
-  it "is invalid without weight" do
-  	expect(FactoryGirl.build(:exercise_set, weight: nil)).to_not be_valid
-  end
-  it "is invalid without reps" do
-  	expect(FactoryGirl.build(:exercise_set, reps: nil)).to_not be_valid
-  end
+
+  let (:exercise_set) { FactoryGirl.build(:exercise_set) }
+
+  it { expect(exercise_set).to be_valid }
+  	
   it { is_expected.to belong_to(:exercise) }
   it { is_expected.to validate_presence_of(:exercise).with_message('must exist') }
+  it { is_expected.to validate_presence_of(:weight) }
+  it { is_expected.to validate_presence_of(:reps) }
 end
