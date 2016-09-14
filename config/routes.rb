717e6b4875a360a0bd55c93b1f35b2f0a323b586
Julio-Home <julio.ss@gmail.com>
2016-09-14 09:58:04 -0300

    adds login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'programs#index'
   get 'signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy' 
 
   resources :programs do
     resources :cycles do
