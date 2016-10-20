c809d82f722b321e5873998653a2f9d6b44f2e61
Julio-Home <julio.ss@gmail.com>
2016-10-20 16:12:15 -0200

    updates gemfile and removes unused files for an rails-api

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  has_many :programs, dependent: :destroy
-  has_secure_password
-  validates_presence_of :first_name, :email, :password
-  validates_uniqueness_of :email
-end
